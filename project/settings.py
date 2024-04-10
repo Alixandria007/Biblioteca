17fd790f2e964d81193bd18f8bb5be0d7cfb377f
Alixandria007 <juninhokrauts778@gmail.com>
2024-04-09 21:09:10 -0300

    Adicionei o app 'Emprestimos' ao projeto

--- project/settings.py
+++ project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.livros'
+    'apps.livros',
+    'apps.emprestimos'
 ]
 
 MIDDLEWARE = [
