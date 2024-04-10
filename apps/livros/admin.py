8fcb612eaebed24613c2ee0dc20aecbf2a9777b3
Alixandria007 <juninhokrauts778@gmail.com>
2024-04-09 22:01:24 -0300

    Adicionei as admins de 'Livros', 'Genero' e 'Autor' ao app 'Livros'

--- apps/livros/admin.py
+++ apps/livros/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+@admin.register(models.Autor)
+class AutorAdmin(admin.ModelAdmin):
+
+    list_display = 'id' , 'nome'
+    list_display_links = 'id',
+    list_per_page = 10
+    ordering = '-id',
+
+@admin.register(models.Genero)
+class GeneroAdmin(admin.ModelAdmin):
+
+    list_display = 'id' , 'nome'
+    list_display_links = 'id',
+    list_per_page = 10
+    ordering = '-id',
+
+@admin.register(models.Livros)
+class LivrosAdmin(admin.ModelAdmin):
+
+    list_display = 'id', 'nome' , 'paginas' , 'autor' , 'genero'
+    list_display_links = 'id',
+    list_editable  = 'nome', 'paginas'
+    list_per_page = 10
+    ordering = '-id',
+
+    prepopulated_fields = {
+        'slug' : ('nome',),
+    }
+
